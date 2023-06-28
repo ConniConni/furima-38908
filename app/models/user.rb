d2c2803f31aef17b72f2e94cbbad7a62458512ce
ConniConni <nmtgktmo@gmail.com>
2023-06-28 16:31:49 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :firstname_kana, presence: true
-  validates :lastname_kana, presence: true
+  validates :firstname, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :lastname, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :firstname_kana, presence: true ,format:{ with: /\A[ァ-ヶー－]+\z/ }
+  validates :lastname_kana, presence: true ,format:{ with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 end
