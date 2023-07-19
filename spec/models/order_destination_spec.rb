c25b42175992a6d21c1cb91912295f7aa6b74610
ConniConni <nmtgktmo@gmail.com>
2023-07-19 17:43:03 +0900

    商品購入機能修正4

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -73,6 +73,17 @@ RSpec.describe OrderDestination, type: :model do
         @order_destination.valid?
         expect(@order_destination.errors.full_messages).to include("Token can't be blank")
       end
+      it "userが紐付いていない場合は登録できないこと" do
+        binding.pry
+        @order_destination.user_id = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("User can't be blank")
+      end  
+      it "itemが紐付いていない場合は登録できないこと" do
+        @order_destination.item_id = nil
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
