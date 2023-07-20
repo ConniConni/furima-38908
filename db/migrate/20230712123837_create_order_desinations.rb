fb1b313104d5e1211d7c988624b9f042becdd515
ConniConni <nmtgktmo@gmail.com>
2023-07-20 23:56:56 +0900

    データベース修正

--- db/migrate/20230712123837_create_order_desinations.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrderDesinations < ActiveRecord::Migration[6.0]
-  def change
-    create_table :order_desinations do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_12_123837) do
+ActiveRecord::Schema.define(version: 2023_07_11_124842) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -65,11 +65,6 @@ ActiveRecord::Schema.define(version: 2023_07_12_123837) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "order_desinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
