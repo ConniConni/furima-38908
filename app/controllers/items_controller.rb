27c860132fe434eaebe2150e6dc12b1b09158f1c
ConniConni <nmtgktmo@gmail.com>
2023-07-21 10:45:16 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_sign_in, only: [:edit, :destroy]
 
   def index
